b48be5b592398e1bc5ee816ebcb260a4ab794527
krjmrok <kriku2001@gmail.com>
2026-01-05 20:54:20 +0900

    文字カウントAppの作成 DOM操作での色変更

--- wordcount/js/count.js
+++ wordcount/js/count.js
@@ -1,12 +1,16 @@
 const text = document.querySelector("#text");
 const count = document.querySelector("#count");
 
+
 text.addEventListener("keyup",() => {
     count.textContent = text.value.length;
+    const textLength = text.value.length;
 
-    if(text.value.length > 100) {
-        count.classList.add("alert");
-    }else {
-        count.classList.remove("alert");
-    }
-});
+    if (textLength === 777) {count.style.color = "gold";
+    }else if (textLength > 200) {count.style.color = "blue";
+    }else if (textLength > 100) {count.classList.add("alert");
+    }else {count.classList.remove("alert");
+    }});
+
+/*「"文字列".length;」👈chromeでコンソールに直打ちすると3で返してくれる。
+「[..."😊"].length」で1と返してくれる。👈上と同じように書くと上手く返してくれない場合。*/
