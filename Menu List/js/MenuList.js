fdd22ee6f481cb0c4cf40914b6c47ad31e69346c
krjmrok <kriku2001@gmail.com>
2026-01-21 21:15:54 +0900

    MenuList完成

--- Menu List/js/MenuList.js
+++ Menu List/js/MenuList.js
@@ -4,7 +4,7 @@ const lists = [
     {
         name: "お好み焼き",
         img: "お好み焼き.jpg",
-        price: 800,
+        price: 1000,
     },
     {
         name: "ざるそば",
@@ -34,7 +34,7 @@ const lists = [
 ];
 
 for (let i = 0 ; i < lists.length; i++ ) {
-    const {name, img, price} = lists[i];
+    const {name, img, price} = lists[i];//👈分割代入
     const content = `<div><img src="images/${img}" alt=""><h2>${name}<h2><p>${price}円</p></div>`;
     menu.insertAdjacentHTML("beforeend",content); //insertAdjacentHTMLを使ってHTMLとして表示させている。
 }
@@ -46,3 +46,66 @@ for (let i = 0 ; i < lists.length; i++ ) {
 // const content = `<div><img src="images/唐揚げ定食.jpg" alt=""></div>`;
 // menu.textContent = content;
 // //HTMLの行がそのまま出力されてしまう。
+
+//--------------------------------------------------------------------------//
+
+// const menu = document.querySelector("#menu");
+
+// const lists = [
+//     "お好み焼き.jpg",
+//     "ざるそば定食.jpg",
+//     "スタミナ定食.jpg",
+//     "トンカツ定食.jpg",
+//     "ラーメン.jpg",
+//     "唐揚げ定食.jpg",
+// ];
+// console.log(lists.length);
+
+// for(let i = 0; i < lists.length; i++){
+//     const content = `<div><img src="images/${lists[i]}" alt=""></div>`;
+//     menu.insertAdjacentHTML("beforeend", content);
+// }
+
+//--------------------------------------------------------------------------//
+
+// //letで再代入!!
+// let letName = "マナ";
+// letName = "メアリー"//👈再代入している
+// console.log(letName);
+
+
+// //constで再代入!!
+// const constName = "大本";
+// // constName = "高橋";//👈定数を書き換えようとしてる。(エラー文：Uncaught TypeError: Assignment to constant variable.at MenuList.js:79:11このエラーを分析)
+// console.log(constName);
+
+// //constを使う理由を覚えること！！
+
+//--------------------------------------------------------------------------//
+
+//オブジェクトの情報を取得
+
+// const menu = document.querySelector("#menu");
+
+// const okonomiyaki = {
+//     name:"お好み焼き",
+//     img:"お好み焼き.jpg",
+//     price:1000
+// };
+// const key = "name";
+// console.log(okonomiyaki[key]);
+
+// const lists = [
+//     "お好み焼き.jpg",
+//     "ざるそば定食.jpg",
+//     "スタミナ定食.jpg",
+//     "トンカツ定食.jpg",
+//     "ラーメン.jpg",
+//     "唐揚げ定食.jpg",
+// ];
+
+
+// for(let i = 0; i < lists.length; i++){
+//     const content = `<div><img src="images/${lists[i]}" alt=""></div>`;
+//     menu.insertAdjacentHTML("beforeend", content);
+// }
