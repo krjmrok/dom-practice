022f60dfd610e9f9e2f508ba58ed04b932053e2f
krjmrok <kriku2001@gmail.com>
2026-03-11 22:19:25 +0900

    forEach文で画像をマウスで選択＆表示

--- WebPage/js/script.js
+++ WebPage/js/script.js
@@ -106,12 +106,44 @@ window.addEventListener("load",() => { //windowで"load"されたのを確認し
 const mainImage = document.querySelector(".gallery-image img");
 const thumbImages = document.querySelectorAll(".gallery-thumbnails img");
 
-thumbImages.forEach((thumbImage) => {
+//forEach文での書き方
+// thumbImages.forEach((thumbImage) => {
+//     thumbImage.addEventListener("mouseover",(event) => {
+//         mainImage.src = event.target.src;
+//         mainImage.animate({opacity: [.6,1]}, 500);
+//     });
+// });
+
+//for文での練習
+// for(let i = 0; i < thumbImages.length; i++) {
+//     thumbImages[i].addEventListener("mouseover",(event) => {
+//         console.log(event.target.src);
+//         mainImage.animate({opacity: [0 , 1]}, 500);
+//     })
+// }
+
+//forEach文での書換え
+thumbImages.forEach((thumbImage) => {//forEachでthumbImagesの中身を一つずつthumbImageに代入してくれる
+    // console.log(thumbImage.src);//thumbImageのなかのsrcをしていしてコンソールに表示
     thumbImage.addEventListener("mouseover",(event) => {
         mainImage.src = event.target.src;
-        mainImage.animate({opacity: [0,1]}, 800);
-    });
-});
+        mainImage.animate({opacity: [0 , 1]}, 500);
+    })
+})
 
-//練習
-console.log(thumbImages);
+
+
+//ForEachでの練習
+const animals = ["猫","牛","虎","兎"];
+
+//無名関数の場合
+// animals.forEach((animal) => {
+//     console.log(animal);
+// })
+
+//関数名をつける場合
+const showAnimal = (animal) => {
+    console.log(animal);
+};
+
+animals.forEach(showAnimal);
